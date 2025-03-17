1983740f71fae3b18f1551ecfc9ecbbf209965d7
Sayed Rokibul Hossain <jaky1206@yandex.com>
2025-03-18 01:00:26 +0700

    ♻️ refactor(tool): rename file for multi-agent support
    - rename llamaindex-agentic-rag-query-engine-tool.py to multi-agent-agentic-rag-query-engine-tool.py
    - add multi agent support

--- multi-agent-agentic-rag-query-engine-tool.py
+++ multi-agent-agentic-rag-query-engine-tool.py
@@ -11,6 +11,7 @@ from llama_index.core import VectorStoreIndex
 from llama_index.llms.openai import OpenAI
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.tools import QueryEngineTool
+import asyncio
 from llama_index.core.agent.workflow import AgentWorkflow
 
 load_dotenv()
@@ -66,11 +67,57 @@ query_engine_tool = QueryEngineTool.from_defaults(
 query_engine_agent = AgentWorkflow.from_tools_or_functions(
     [query_engine_tool],
     llm=llm,
-    system_prompt="You are a helpful assistant that has access to a database containing informatrion about transformer models."
+    system_prompt="You are a helpful assistant that has access to a database containing informatrion about transformer models.",
 )
 
-query = "What does attention mean for transformer models?"
+# query = "What does attention mean for transformer models?"
+# response = query_engine.query(query)
+# print(response)
 
-response = query_engine.query(query)
+from llama_index.core.agent.workflow import (
+    AgentWorkflow,
+    ReActAgent,
+)
 
-print(response)
+
+# Define some tools
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers."""
+    return a - b
+
+
+# Create agent configs
+# NOTE: we can use FunctionAgent or ReActAgent here.
+# FunctionAgent works for LLMs with a function calling API.
+# ReActAgent works for any LLM.
+calculator_agent = ReActAgent(
+    name="calculator",
+    description="Performs basic arithmetic operations",
+    system_prompt="You are a calculator assistant. Use your tools for any math operation.",
+    tools=[add, subtract],
+    llm=llm,
+)
+
+query_agent = ReActAgent(
+    name="info_lookup",
+    description="Looks up information about XYZ",
+    system_prompt="Use your tool to query a RAG system to answer information about XYZ",
+    tools=[query_engine_tool],
+    llm=llm,
+)
+
+# Create and run the workflow
+agent = AgentWorkflow(agents=[calculator_agent, query_agent], root_agent="calculator")
+
+async def main():
+    # Run the system
+    response = await agent.run(user_msg="Can you add 5 and 3?")
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
